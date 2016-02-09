5d7bfacb12074a5b795fa5ff864b06861b9d553d
Michael Sandula <msandula@gmail.com>
2016-02-09 13:19:20 -0500

    Lab Seven: Grocery list

---
+++ labSeven.js
@@ -0,0 +1,34 @@
+var groceryList  = [
+	{
+		name: "milk",
+		price: 2.79
+	},
+	{
+		name: "bread",
+		price: 3.49
+	},
+	{
+		name: "bananas",
+		price: 3.19
+	},
+	{
+		name: "chicken breasts",
+		price: 11.42
+	},
+	{
+		name: "yogurt",
+		price: 5.69
+	}
+];
+
+for (var item in groceryList) {
+	console.log(groceryList[item].name);
+	console.log(groceryList[item].price);
+}
+
+var total = 0;
+for (var p in groceryList) {
+		total += groceryList[p].price;
+}
+
+console.log("Total: " + total);
